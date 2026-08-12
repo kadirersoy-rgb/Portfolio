18e0b4bb15739c49d757e59970767d64ac6d6745
kadirersoy-rgb <kadir.ersoy@edu.esiee.fr>
2026-08-13 01:37:41 +0200

    Construction Hero en cours

---
+++ components/Hero.jsx
@@ -0,0 +1,25 @@
+export const Hero = () => {
+  return (
+    <section id="home" className="hero">
+      <div className="hero-content">
+
+        <div className="hero-left">
+          <p>Hello, I'm</p>
+
+          <h1>
+            Kadir
+            <br />
+            Ersoy
+          </h1>
+        </div>
+
+        <div className="hero-right">
+          <p>Computer Engineering Student at ESIEE Paris</p>
+          <h2>Software Engineer</h2>
+          <span>at Brasserie Champigneulles</span>
+        </div>
+
+      </div>
+    </section>
+  );
+};
